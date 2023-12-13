0401289163e64f4f2cb652a580adcf8945a53db2
Amit <amitpardhan589@gmail.com>
2023-12-13 11:15:04 +0530

    config colors in tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#8FB78B", // Soft Green
+        heroBackground: "#AED9E0", // Light Blue
+        textContrast: "#FFFFFF", // White (for text on hero background)
+        heading: "#2D3748", // Dark Grey (for headings)
+        button: "#6B7280", // Grey (for buttons)
+        border: "#CBD5E0", // Light Grey (for borders and dividers)
+        heroHeading: "#2D3748",
+      },
+    },
   },
   plugins: [],
 };
